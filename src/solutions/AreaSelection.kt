325d154476399c5350b07d72597fb4510b9c07b1
Arch-vile <mikko.ravimo@gmail.com>
2018-12-30 16:07:35 +0200

    TEMP

--- src/solutions/AreaSelection.kt
+++ src/solutions/AreaSelection.kt
@@ -2,6 +2,7 @@ package solutions
 
 import model.Location
 import model.Route
+import model.SLEIGHT_CAPACITY
 import utils.*
 
 /**
@@ -27,13 +28,36 @@ class AreaSelection(setup: List<Location>) {
 
         while (locations.isNotEmpty()) {
             println("Pending locations ${locations.size}")
+
+            // First fill the sleight mostly from the current location
             val center = findFurthest(KORVATUNTURI, locations)
-            val area = expandUntilFilled(center, locations, 98, 0)
+            val area = expandUntilFilled(center, locations, 98, 0).toMutableList()
+            locations.removeAll(area)
+
+            // Then fill remaining while heading home
+            locations.sortBy {
+                distance(center, it) + distance(KORVATUNTURI, it)
+            }
+            var weight = sleightWeight(Route(area))
+            for (location in locations) {
+                // TODO: Calculating distance multiple times here
+                // Lets stop adding when we get too far of the return path
+                if (distance(center, location) + distance(KORVATUNTURI, location) > distance(center, KORVATUNTURI) * 1.1) {
+                    break
+                }
+
+                if(weight + location.weight < SLEIGHT_CAPACITY) {
+                    area.add(location)
+                    weight += location.weight
+                }
+            }
+
 
             val route = shortestRouteFromKorvatunturiLooping(area)
-            locations.removeAll(route.stops)
             routes.add(route)
         }
+
+
 
         return routes
     }
